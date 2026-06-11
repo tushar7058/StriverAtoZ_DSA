75f3da9f14f9e6501f545d128918a38d2c08a482
tushar7058 <kaletushar991@gmail.com>
2026-06-11 07:37:25 +0530

    pattern5 added

---
+++ Basic/patterns/pattern5.py
@@ -0,0 +1,31 @@
+"""
+
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+
+
+
+"""
+
+n = int(input("Enter a value : "))
+def printpattern5(n):
+    for i in range(n , 0 , -1):
+        for j in range(1 , i+1):
+            print(j , end=" ")
+
+        print()
+
+printpattern5(n)
+
+# n = int(input("Enter a value : "))
+
+# def printpattern5(n):
+#     for i in range(n, 0, -1):   # from n to 1
+#         for j in range(1, i + 1):
+#             print(j, end=" ")
+#         print()
+
+# printpattern5(n)
